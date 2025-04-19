f9e7038c1d50c8fad2056552a45a055ad075cc95
Yarikscode <148035045+Yarikscode@users.noreply.github.com>
2025-04-19 18:01:06 +0700

    Add files via upload

---
+++ modules/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 1.00"
+}
+
+
+provider "aws" {
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+}
